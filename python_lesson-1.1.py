5668549c2fa07e8ebf5c3733bad9d61367846d69
resul_1903 <bjkresul571@gmail.com>
2022-04-12 19:02:44 +0300

    python lesson notes-1.1

---
+++ python_lesson-1.1.py
@@ -0,0 +1,23 @@
+name ="resul"
+age = "young"
+gender = "male"
+school = "clarusway"
+output= f"{name} is {age} and he goes to {school} " 
+print(output)
+
+
+ad = "ALİ veli"
+ad_2 =ad.capitalize()
+print(ad)
+print(ad.swapcase())
+print(ad_2)
+
+ad = "benim adım REsul"
+print(ad.replace("a","x"))
+e_mail = "clarusway@clarusway.co  is my mail address"
+print(e_mail.startswith("r",3))
+
+
+text = "the better the family, the better the society"
+text = text.title()
+print(text)
